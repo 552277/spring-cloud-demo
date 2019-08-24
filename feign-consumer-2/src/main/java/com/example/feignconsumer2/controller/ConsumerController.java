be17d358e907ab7d1ba0515c99876878252d5335
weichangzhong <weichangzhong@sohu-inc.com>
2019-08-24 23:49:46 +0800

    使用sleuth追踪日志链路

--- feign-consumer-2/src/main/java/com/example/feignconsumer2/controller/ConsumerController.java
+++ feign-consumer-2/src/main/java/com/example/feignconsumer2/controller/ConsumerController.java
@@ -1,19 +1,27 @@
 package com.example.feignconsumer2.controller;
 
 import com.example.feignconsumer2.service.HelloService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class ConsumerController {
+
+    Logger logger = LoggerFactory.getLogger(ConsumerController.class);
 
     @Autowired
     HelloService helloService;
 
     @RequestMapping(value = "/feign-consumer", method = RequestMethod.GET)
     public String helloController() {
-        return helloService.hello();
+        logger.info("Request start...");
+        String res = helloService.hello();
+        logger.info("The Request end, response [{}]", res);
+        return res;
     }
 }
